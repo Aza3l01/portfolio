fe5fa84d6147f7de8a60b592789582634165c8b9
Aza3l01 <aza3l0309@gmail.com>
2025-08-21 07:30:37 +0530

    just testing out css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 const page = () => {
   return (
-    <main className="flex justity-center bg-zinc-800">
-      <div>
+    <main className="flex bg-zinc-800">
+      <div className="flex justify-center flex-col m-auto h-screen mt-4">
         <img src="/bgcat.jpg" className=""/>
-        <h1>azael</h1>
-        <h1>UNDER CONSTRUCTION</h1>
+        <div className="flex justify-center flex-col m-auto h-screen">
+          <h1 className="text-center">azael</h1>
+          <h1 className="text-center">UNDER CONSTRUCTION</h1>
+        </div>
       </div>
     </main>
   )
