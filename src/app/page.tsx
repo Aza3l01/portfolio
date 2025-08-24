3b33514ff93d8d957b44bbd572c72ecac47b4308
Aza3l01 <aza3l0309@gmail.com>
2025-08-24 17:53:33 +0530

    changed/added header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
+import Header from '@/components/Header'
 import React from 'react'
 
 const page = () => {
   return (
-    <main className="flex bg-zinc-800">
-      <div className="flex justify-center flex-col m-auto h-screen mt-4">
-        <img src="/bgcat.jpg" className=""/>
-        <div className="flex justify-center flex-col m-auto h-screen">
-          <h1 className="text-center">azael</h1>
-          <h1 className="text-center">UNDER CONSTRUCTION</h1>
-        </div>
-      </div>
+    <main className="bg-zinc-800">
+        <Header/>
     </main>
   )
 }
